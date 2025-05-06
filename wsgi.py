b48da303bc55cd1a5971d4874f60b9f16f647c65
Sijie Yu <sjyu1988@gmail.com>
2025-05-06 13:05:11 -0400

    Refactor WSGI application run configuration

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 #this is the wsgi.py
 from routes import app
 if __name__ == "__main__":
-    # app.run()
-    app.run(debug=True, port=2001)
+    app.run()
+    # app.run(debug=True, port=2001)
 
 # from run import app
 # if __name__ == "__main__":
