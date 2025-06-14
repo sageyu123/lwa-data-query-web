86291a78d35cf6d85ecb6107713a4acaeaaabe4d
Xingyao Chen <109064035+xingyaochen0@users.noreply.github.com>
2025-06-14 13:49:55 -0400

    Update run_queryweb_daily.sh

--- utils/run_queryweb_daily.sh
+++ utils/run_queryweb_daily.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source ~/.bashrc
+source /home/xychen/pyenvx/bin/activate
+
 sh_path="/home/xychen/lwa-data-query-web/utils"
 
 # Date, "YYYY-MM-DD" format for the daily movie generation
@@ -10,7 +13,6 @@ end_date=$(date -d "today" +%Y-%m-%d)
 start_datetime=$(date -d "yesterday" +%Y-%m-%dT12:00:00)
 end_datetime=$(date -d "today" +%Y-%m-%dT03:00:00)
 
-# Run the commands (note the correct variable usage)
+# Run the commands
 # python "$sh_path/lwa-query-web_utils.py" --gen movie --start "$start_date" --end "$end_date"
 python "$sh_path/lwadata2sql.py" --start "$start_datetime" --end "$end_datetime"
-
